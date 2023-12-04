92ce9fe3086c8900a14240fdff3225a9090e9b8b
Gael Cadoret <gaelcadoret21@gmail.com>
2023-12-04 18:36:07 +0100

    add customJsonParser

---
+++ src/modules/jsonParser/index.ts
@@ -0,0 +1,73 @@
+
+
+const startsWith = (char) => (str) => str.toString().startsWith(char)
+const startsWithAlias = startsWith('@');
+const startsWithJsonPath = startsWith('$.');
+const isObject = (val) => !Array.isArray(val) && typeof val === 'object'
+const isArray = (val) => Array.isArray(val)
+const mapAndParse = (val, lvl) => val.map((el, idx) => jsonParser(el, lvl, idx))
+const split = (separator) => (str) => str.split(separator);
+const splitByColon = split(":");
+
+const extractContextAndId = (val) => splitByColon(val)
+
+const parseLevelReducer = (lvl, arrayIdx) => (acc, [key, val]) => {
+    if (arrayIdx != null) console.log(`%c arrayIdx %c => ${arrayIdx}`, 'background-color:#f2b7b3;font-weight:bold;color:white;', 'background-color:white;color:gray;font-style:italic;')
+
+    console.log('key', key);
+    console.log('val', val);
+
+    // manage jsonPath
+    if (startsWithJsonPath(val)) {
+        console.log(`%c find jsonPath %c => ${val}`, 'background-color:orange;font-weight:bold;color:white;', 'background-color:white;color:gray;font-style:italic;')
+    }
+
+    // manage alias in key
+    if (startsWithAlias(key)) {
+        console.log(`%c find alias (key) %c => ${key}`, 'background-color:lightblue;font-weight:bold;color:white;', 'background-color:white;color:gray;font-style:italic;')
+        console.log('val', val)
+
+        if (key === "@id") {
+            console.log(`%c find alias (key) @id %c => ${key}`, 'background-color:lightblue;font-weight:bold;color:white;', 'background-color:white;color:gray;font-style:italic;')
+            const [context, id] = extractContextAndId(val)
+            console.log("[context, id]", [context, id])
+        }
+    }
+
+    // manage alias in value
+    if (startsWithAlias(val)) {
+        console.log(`%c find alias (val) %c => ${val}`, 'background-color:lightblue;font-weight:bold;color:white;', 'background-color:white;color:gray;font-style:italic;')
+        console.log('acc', acc)
+    }
+
+    // manage arrays
+    if (isArray(val)) {
+        console.log(`%c find array %c => ${key}`, 'background-color:yellow;font-weight:bold;color:#333333;', 'background-color:white;color:gray;font-style:italic;')
+        return {
+            ...acc,
+            [key]: mapAndParse(val, lvl + 1),
+        }
+    }
+
+    // manage objects
+    if (isObject(val)) {
+        console.log(`%c find object %c => ${key}`, 'background-color:lightgreen;font-weight:bold;color:#333333;', 'background-color:white;color:gray;font-style:italic;')
+
+        return {
+            ...acc,
+            [key]: jsonParser(val, lvl + 1),
+        }
+    }
+
+    return {
+        ...acc,
+        [key]: val,
+    }
+}
+
+const jsonParser = (obj, lvl = 0, arrayIdx = null) => {
+    console.log('%clvl => %c ' + lvl + " ", 'color: gray', 'background-color:#00CC00;color:white')
+    return Object.entries(obj).reduce(parseLevelReducer(lvl, arrayIdx), {});
+}
+
+export default jsonParser
---
+++ src/modules/jsonParser/jsonParser.test.ts
@@ -0,0 +1,72 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert'
+
+import jsonParser from './'
+
+describe('modules/jsonParser', () => {
+    it('should parse json correctly without changing the structure of data', () => {
+        // Given
+        const jsonObj = {
+            "@context": { // lvl 0
+                "products": { // lvl 1
+                    "version": "1.0"  // lvl 2
+                }
+            },
+            "templates": [  // lvl 0
+                {
+                    "@id": "product:XXXXXXXXXXXXX",
+                    "url": "@url_to_replace", // lvl 1
+                    "name": "template n°1 (mobile)", // lvl 1
+                    "type": "@device_type"
+                },
+                {
+                    "url": "@url_to_replace", // lvl 1
+                    "name": "template n°2 (desktop)", // lvl 1
+                    "description": "template n°2", // lvl 1
+                    "options": { // lvl 1
+                        "bulletPoints": { // lvl 2
+                            "brand": "audi", // lvl 3
+                            "model": "A7 sportback",
+                            "description": "$.products.[0].description"
+                        }
+                    }
+                },
+                {
+                    "url": "@url_to_replace", // lvl 1
+                    "name": "template n°3 (tablet)", // lvl 1
+                    "description": "template n°3", // lvl 1
+                    "options": { // lvl 1
+                        "bulletPoints": { // lvl 2
+                            "brand": "audi", // lvl 3
+                            "model": "A7 sportback",
+                            "description": "$.products.[0].description"
+                        }
+                    }
+                },
+                {
+                    "url": "@url_to_replace", // lvl 1
+                    "name": "template n°4 (iPhone)", // lvl 1
+                    "description": "template n°4", // lvl 1
+                    "options": { // lvl 1
+                        "bulletPoints": { // lvl 2
+                            "brand": "audi", // lvl 3
+                            "model": "A7 sportback",
+                            "description": "$.products.[0].description"
+                        }
+                    }
+                }
+            ],
+            "media": { // lvl 0
+                "videos": { // lvl 1
+                    "@title": "video_title" // lvl 2
+                }
+            }
+        }
+
+        // When
+        const result = jsonParser(jsonObj)
+
+        // Then
+        assert.deepStrictEqual(result, jsonObj)
+    })
+})
